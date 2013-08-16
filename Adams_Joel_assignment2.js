b531338c976739fbea8a006ea6c7369eb0e9c913
Joel Adams <jadams924@fullsail.edu>
2013-08-16 03:35:34 -0400

    1

--- Adams_Joel_assignment2.js
+++ Adams_Joel_assignment2.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+
+var crewName= "Wesley";
+var DaysSailing= 8;
+var decks=["Bridge", "Gun deck", "Main deck", "Quarterdeck" ];
+var decksRemaining = decks.length;
+
+document.writeln("CREW MEMBER NAME:" + crewName );
+document.writeln("<br/>");
+
+document.writeln("NUMBER OF DAYS SAILING:" + DaysSailing);
+document.writeln("<br/>");
+
+document.writeln("DECKS REMAINING TO SWAB:" + decksRemaining);
+document.writeln("<br/>");
+var i=0;
+
+function Swab()
+{
