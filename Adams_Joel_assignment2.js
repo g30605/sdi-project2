54b3b889320a78edff69b1f85d2c144506f57f81
Joel Adams <jadams924@fullsail.edu>
2013-08-16 22:48:25 -0400

    7

--- Adams_Joel_assignment2.js
+++ Adams_Joel_assignment2.js
@@ -91,3 +91,72 @@ var otherSurvivors = function(survivor1, survivor2) {
 	return survivor1;
 	return survivor2;
 };
+//String
+var getRescued = function() {
+	var flare = flaresAndFood[0],
+		food = flaresAndFood[1];
+	console.log("Launching the first "+
+				flare +
+				"!!!"
+	);
+	for (var flare = 10, food = 30; flare && food >= 0; flare--, food-=3) {
+		console.log("Flare up! There are " +
+					flare +
+					" flares left and "+
+					food +
+					" cans of food left!");
+	};
+	console.log("On the last day that they could have stayed on that building the trio heard a " +
+				"loud whirring noise. It was the helicopter coming back to save them!"
+	);
+	return flare;
+	return food;
+};
+
+
+//Story
+console.log(dudeName + " is one of the last surviving humans on earth." +
+			"  Every morning when he gets up he tries to find breakfast" +
+			" nearby before going zombie hunting."
+);
+findBreakfast("Fry's", 10, 1);
+console.log("While wandering around the deserted city of Winter Park, " +
+			dudeName +
+			" comes across a group of zombies."
+);
+zombieGroup(65, 20);
+console.log("He then returns to his aimless treck through the deserted city.");
+console.log("Not long after his first zombie encouner, " +
+			dudeName +
+			" finds himself stopped by essentially a barricade of unavoidable living dead."
+);
+console.log("Unable to run anywhere, " +
+				dudeName +
+				", pulls out his trusty " +
+				weapons[1] +
+				" and starts shooting at the writhing mass of undead"
+		);
+zombieRoadBlock(90);
+console.log("After the massacring of the zombie blockade, " +
+			dudeName +
+			" sees two actual people walking down the street towards him." + 
+			"  Cautiously he approaches them."
+);
+otherSurvivors("Danielle", "Anthony");
+console.log("As the trio moves through the city they hear the sound of a helicopter nearby.  " +
+			"They walk carefully but quickly through the city in search for where it could be " +
+			"and manage to catch a glimpse of it as it lifts off from a roof and leaves the area."
+);
+console.log("Even though it was gone, they all had hope for rescue. " +
+			"They entered a tall building near them so that they could try to flag down the next " +
+			"chopper to fly by.  Lucky for them on the way up they found not only food but also " +
+			"flares."
+);
+console.log("They decide that they should launch one flare a day.  " + 
+			"They also decide that since there are 10 flares and 30 cans of food, " + 
+			"that they should also only eat one can of food each per day."
+);
+getRescued();
+console.log("The helicopter picked them up off the top of that building and carried them away to " +
+			"the last know zombie free place in the world, Alaska."
+);
