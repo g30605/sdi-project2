9821d830d6cef5a308b82ec8a66fc4889f0cbb0a
Joel Adams <jadams924@fullsail.edu>
2013-08-16 22:39:44 -0400

    6

--- Adams_Joel_assignment2.js
+++ Adams_Joel_assignment2.js
@@ -49,3 +49,45 @@ var zombieGroup = function(numberOfBullets, numberOfZombies) {
 	return numberOfBullets,
 		numberOfZombies;
 };
+//Boolean
+var zombieRoadBlock = function(numberOfZombies) {
+	while (numberOfZombies && shotgunShells >= 0) {
+		console.log(numberOfZombies + 
+					" zombies left and " + 
+					shotgunShells +
+					" shells left!"
+		);
+		numberOfZombies-=10;
+		shotgunShells-=2; 
+	};
+	return numberOfZombies;
+};
+//Number
+var otherSurvivors = function(survivor1, survivor2) {
+	console.log("As the two people get closer, " +
+				dudeName +
+				" tenses up and prepares to use his " +
+				weapons[2] +
+				" in case they try to rob him."
+	);
+	console.log("The two survivors notice his tension and put their hands up to show " +
+				"that they mean no harm."
+	);
+	console.log("The couple is now close enough that " +
+				dudeName +
+				" can tell that one is a man and the other a woman, " +
+				"both similar in appearance and age."
+	);
+	console.log("Finally they get to him and introduce themselves as " +
+				survivor1 +
+				" and " +
+				survivor2 +
+				".  They are brother and sister and are looking for rescue."
+	);
+	console.log(dudeName +
+				"asks them if they would like to join him to travel as a group, " +
+				'they reply with a quick but thankful "Yes!"'
+	);
+	return survivor1;
+	return survivor2;
+};
